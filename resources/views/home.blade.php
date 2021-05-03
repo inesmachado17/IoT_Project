410f844d241bdcb7afb9169f6e1de24d37103830
inesmachado17 <ines.machado17@gmail.com>
2021-05-03 16:22:52 +0000

    home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,12 +6,12 @@
 @section('content')
 <div class="row mb-5">
     <div class="col">
-        <div class="w3-container w3-center w3-animate-top">
+        <div class="w3-container w3-center w3-animate-top w3-animate-fading" style="animation-duration: 1s">
             <h1 class="text-center">Smart Home</h1>
         </div>
         @auth
         @else
-            <div class="w3-container w3-center w3-animate-left">
+            <div class="w3-container w3-center w3-animate-left w3-animate-fading" style="animation-duration: 1s">
                 <a href="{{ route('login') }}" class="btn btn-outline-primary">Log in</a>
             </div>
         @endauth
@@ -20,7 +20,7 @@
 
 <div class="row d-flex justify-content-center">
     <div class="col-6">
-        <div class="w3-container w3-center w3-animate-bottom">
+        <div class="w3-container w3-center w3-animate-bottom w3-animate-fading" style="animation-duration: 1s">
             <img id="smart-home-welcome" src="/imgs/smart-home.png" alt="Casa Inteligente">
         </div>
     </div>
